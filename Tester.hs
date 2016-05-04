ab9c80bb8dc54e49940e87330e8861b613e9cac8
Tim Elvart <telvart@ku.edu>
2016-05-03 21:27:57 -0500

    commit

--- Tester.hs
+++ Tester.hs
@@ -33,6 +33,11 @@ ringList = [[0..6],
 
 sudokuBoard= [5,0,0,0,1,6,0,0,0,0,3,0,0,0,7,0,6,2,1,0,0,0,1,7,0,0,6,0,0,5,0,3,6,7,2,0,0,2,1,0,0,4,0,0,4,0,0,1,0]
 
+
+
+main = putStrLn ( show $lotusSolver sudokuBoard )
+
+
 determineRing:: Int -> Int
 determineRing boardIndex
   | boardIndex > 48 = -1
